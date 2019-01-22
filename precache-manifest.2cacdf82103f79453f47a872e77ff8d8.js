2aaa04c33bf19fbdb34bcf03a901b29f779649de
Ezequiel Espinoza <ezequielnoza@gmail.com>
2019-01-22 10:25:15 -0500

    Updates

---
+++ precache-manifest.2cacdf82103f79453f47a872e77ff8d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0072f799df4360a84c35",
+    "url": "/corkboard-to-do/static/js/runtime~main.0072f799.js"
+  },
+  {
+    "revision": "dbff8fa2a8372e4d714f",
+    "url": "/corkboard-to-do/static/js/main.dbff8fa2.chunk.js"
+  },
+  {
+    "revision": "07140812a98c93f90dd2",
+    "url": "/corkboard-to-do/static/js/1.07140812.chunk.js"
+  },
+  {
+    "revision": "dbff8fa2a8372e4d714f",
+    "url": "/corkboard-to-do/static/css/main.773f5cd4.chunk.css"
+  },
+  {
+    "revision": "8cee484fc03c81737edd4a5afa3edcc3",
+    "url": "/corkboard-to-do/index.html"
+  }
+];
